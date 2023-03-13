4390af0ea11879a385b660f64fdcb5ce41a2ad0a
extorc <mittalshaurya004.com>
2023-03-13 18:56:58 +0530

    Line numbers

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
 require("extorc")
-vim.wo.relativenumber = true
+require("bufferline").setup{}
+require('vgit').setup()
 
+vim.wo.number = true
+vim.wo.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-
 vim.opt.termguicolors = true
-require("bufferline").setup{}
-vim.keymap.set("n", "<C-q>", vim.cmd.BufferLineCycleNext)
-vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float)
-require('vgit').setup()
--- lua/extorc/remap.lua
+++ lua/extorc/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-q>", vim.cmd.BufferLineCycleNext)
+vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float)
